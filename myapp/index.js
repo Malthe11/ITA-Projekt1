9142eab3899a24822dd1a98e3e8f0ecfec612c33
Malthe11 <malthe.hjort.rasmussen@gmail.com>
2023-05-08 10:00:11 +0200

    database api v2

--- myapp/index.js
+++ myapp/index.js
@@ -1,57 +1,69 @@
 const express = require("express");
+const { Client } = require("pg");
+//postgres://pzshqund:DcBloNSJpyACN6rOZgnuPntOlgr_Yjtb@hattie.db.elephantsql.com/pzshqund
 const app = express();
-const port = 3000;
+const api_port = 3000;
+const klient = new Client({
+  user: "pzshqund",
+  host: "hattie.db.elephantsql.com",
+  database: "pzshqund",
+  password: "DcBloNSJpyACN6rOZgnuPntOlgr_Yjtb",
+  port: 5432,
+});
+const qryA = `SELECT onshorewindpower + offshorewindpower AS windpower FROM energydata WHERE pricearea = 'DK1' LIMIT 5`;
+const qryB = `SELECT fossilgas + fossilhardcoal + fossiloil as notgreen, biomass + hydropower + otherrenewable + solarpower + onshorewindpower + offshorewindpower AS green FROM energydata WHERE pricearea = 'DK1' LIMIT 10`;
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
+app.get("/test", async (req, res) => {
+  res.send("hej");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-app.get("/data", (req, res) => {
-  let data = [
-    { name: "Knud", age: 69 },
-    { name: "Svend", age: 44 },
-    { name: "Malthe", age: 22 },
-    { name: "Kaj", age: 55 },
-  ];
-  res.send(data);
-});
-app.get("/Categories", (req, res) => {
-  getFoodGategories(function (d) {
-    res.send(d);
-  });
+app.get("/Visualisering1", async (req, res) => {
+  try {
+    let queryData = await klient.query(qryA);
+    res.json({
+      ok: true,
+      dataWind: queryData.rows,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      message: error.message,
+    });
+  }
 });
 
-function getFoodGategories(handle) {
-  var pg = require("pg");
+app.get("/Visualisering2", async (req, res) => {
+  try {
+    let queryData = await klient.query(qryB);
+    res.json({
+      ok: true,
+      dataGreen: queryData.rows,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      message: error.message,
+    });
+  }
+});
 
-  var conString =
-    "postgres://pzshqund:DcBloNSJpyACN6rOZgnuPntOlgr_Yjtb@hattie.db.elephantsql.com/pzshqund";
-  var client = new pg.Client(conString);
-  client.connect(function (err) {
-    if (err) {
-      return console.error("could not connect to postgres", err);
-    }
-    //select category, count(category_id) as antal from food_category join food using(category_id) group by category_id
-    //SELECT *, COUNT(dataid) as antal FROM energydata GROUP BY dataid LIMIT 10
-    client.query(
-      "SELECT onshorewindpower + offshorewindpower AS windpower FROM energydata WHERE pricearea = 'DK1' LIMIT 5",
-      function (err, result) {
-        if (err) {
-          return console.error("error running query", err);
-        }
+app.get("/Visualisering3", async (req, res) => {
+  try {
+    let queryData = await klient.query(qryC);
+    res.json({
+      ok: true,
+      foods: queryData.rows,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      message: error.message,
+    });
+  }
+});
 
-        handle(result.rows);
-        client.end();
-      }
-    );
-  });
-}
+// Programmet starter her:
+klient.connect();
+app.listen(api_port, () => {
+  console.log(`App lytter på http://localhost:${api_port}`);
+});
