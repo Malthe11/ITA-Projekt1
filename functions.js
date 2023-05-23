6c2a70a31e33296d8f4d132947b147ac72b435ab
mwt2502 <mwo2502@live.dk>
2023-05-23 11:36:43 +0200

    visu-page formatting

--- functions.js
+++ functions.js
@@ -15,3 +15,35 @@ function nulstilBilledeøst() {
   var billedElement = document.querySelector(".østimage");
   billedElement.style.transform = "scale(1)";
 }
+
+function UpdateVandprocent() {
+  var textElement = document.getElementById("text");
+  textElement.textContent = percentageFromDatabase();
+}
+var percentageFromDatabase = getRandomNumber; // <-- erstattes med funktionen der giver korrekte procent
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 101) + "%";
+}
+document.addEventListener("DOMContentLoaded", function () {
+  var text = new Blotter.Text("72%", {
+    family: "sans-serif",
+    size: 150,
+    fill: "#DCDCDC",
+  });
+
+  var material = new Blotter.LiquidDistortMaterial();
+  material.uniforms.uSpeed.value = 0.9;
+  material.uniforms.uVolatility.value = 0.03;
+  material.uniforms.uSeed.value = 3.02;
+
+  const blotter = new Blotter(material, {
+    texts: text,
+  });
+
+  const scope = blotter.forText(text);
+
+  // Indsæt tekstelementet i HTML
+  const dinmorElement = document.getElementById("vindtekst");
+  scope.appendTo(dinmorElement);
+});
