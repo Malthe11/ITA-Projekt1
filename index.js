4da5b54ce155cd04698db0107278e518a090f2f6
Malthe11 <malthe.hjort.rasmussen@gmail.com>
2023-05-24 14:37:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ const klient = new Client({
 });
 
 const qryA = `SELECT onshorewindpower + offshorewindpower AS windpower, hourdk AS time FROM energydata WHERE pricearea = 'DK1' OFFSET 9 LIMIT 72`;
-const qryB = `SELECT onshorewindpower + offshorewindpower AS windpower, hourdk AS time FROM energydata WHERE pricearea = 'DK1' OFFSET 9 LIMIT 12`;
+const qryB = `SELECT solarpower AS solar, hourdk AS time FROM energydata WHERE pricearea = 'DK1' OFFSET 9 LIMIT 72`;
+const qryG = `SELECT hydropower AS hydro, hourdk AS time FROM energydata WHERE pricearea = 'DK1' OFFSET 9 LIMIT 72`;
+const qryH = `SELECT fossilgas + fossilhardcoal + fossiloil as fossil, hourdk AS time FROM energydata WHERE pricearea = 'DK1' OFFSET 9 LIMIT 72`;
+
+const qryA1 = `SELECT onshorewindpower + offshorewindpower AS windpower, hourdk AS time FROM energydata WHERE pricearea = 'DK2' OFFSET 9 LIMIT 72`;
+const qryB1 = `SELECT solarpower AS solar, hourdk AS time FROM energydata WHERE pricearea = 'DK2' OFFSET 9 LIMIT 72`;
+const qryG1 = `SELECT hydropower AS hydro, hourdk AS time FROM energydata WHERE pricearea = 'DK2' OFFSET 9 LIMIT 72`;
+const qryH1 = `SELECT fossilgas + fossilhardcoal + fossiloil as fossil, hourdk AS time FROM energydata WHERE pricearea = 'DK2' OFFSET 9 LIMIT 72`;
+
 //const qryB = `SELECT fossilgas + fossilhardcoal + fossiloil as notgreen, biomass + hydropower + otherrenewable + solarpower + onshorewindpower + offshorewindpower AS green FROM energydata WHERE pricearea = 'DK1' LIMIT 10`;
 const qryC = `SELECT EXTRACT(HOUR FROM hourdk) AS Time, onshorewindpower + offshorewindpower AS Windpower, hydropower AS hydro, solarpower, fossilgas + fossilhardcoal + fossiloil as fossil FROM energydata WHERE pricearea = 'DK1' OFFSET  9 LIMIT 1`;
 const qryD = `SELECT EXTRACT(HOUR FROM hourdk) AS Time, onshorewindpower + offshorewindpower AS Windpower, hydropower AS hydro, solarpower, fossilgas + fossilhardcoal + fossiloil as fossil FROM energydata WHERE pricearea = 'DK2' OFFSET  9 LIMIT 1`;
@@ -31,12 +39,12 @@ app.get("/test", async (req, res) => {
   res.send("hej");
 });
 
-app.get("/Visualisering1", async (req, res) => {
+app.get("/windV", async (req, res) => {
   try {
     let queryData = await klient.query(qryA);
     res.json({
       ok: true,
-      dataWind: queryData.rows,
+      graf2: queryData.rows,
     });
   } catch (error) {
     res.json({
@@ -46,12 +54,98 @@ app.get("/Visualisering1", async (req, res) => {
   }
 });
 
-app.get("/Visualisering2", async (req, res) => {
+app.get("/solarV", async (req, res) => {
   try {
     let queryData = await klient.query(qryB);
     res.json({
       ok: true,
-      dataGreen: queryData.rows,
+      graf2: queryData.rows,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      message: error.message,
+    });
+  }
+});
+app.get("/hydroV", async (req, res) => {
+  try {
+    let queryData = await klient.query(qryG);
+    res.json({
+      ok: true,
+      graf2: queryData.rows,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      message: error.message,
+    });
+  }
+});
+app.get("/fossilV", async (req, res) => {
+  try {
+    let queryData = await klient.query(qryH);
+    res.json({
+      ok: true,
+      graf2: queryData.rows,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      message: error.message,
+    });
+  }
+});
+
+app.get("/windO", async (req, res) => {
+  try {
+    let queryData = await klient.query(qryA1);
+    res.json({
+      ok: true,
+      graf2: queryData.rows,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      message: error.message,
+    });
+  }
+});
+
+app.get("/solarO", async (req, res) => {
+  try {
+    let queryData = await klient.query(qryB1);
+    res.json({
+      ok: true,
+      graf2: queryData.rows,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      message: error.message,
+    });
+  }
+});
+app.get("/hydroO", async (req, res) => {
+  try {
+    let queryData = await klient.query(qryG1);
+    res.json({
+      ok: true,
+      graf2: queryData.rows,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      message: error.message,
+    });
+  }
+});
+app.get("/fossilO", async (req, res) => {
+  try {
+    let queryData = await klient.query(qryH1);
+    res.json({
+      ok: true,
+      graf2: queryData.rows,
     });
   } catch (error) {
     res.json({
