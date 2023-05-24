4ba110ecde71cc8d8deb32a6758e23a6b85414cb
Malthe11 <malthe.hjort.rasmussen@gmail.com>
2023-05-24 14:04:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const klient = new Client({
   port: 5432,
 });
 
-const qryA = `SELECT onshorewindpower + offshorewindpower AS windpower, hourdk AS time FROM energydata WHERE pricearea = 'DK1' LIMIT 72`;
-const qryB = `SELECT onshorewindpower + offshorewindpower AS windpower, hourdk AS time FROM energydata WHERE pricearea = 'DK1' LIMIT 12`;
+const qryA = `SELECT onshorewindpower + offshorewindpower AS windpower, hourdk AS time FROM energydata WHERE pricearea = 'DK1' OFFSET 20 LIMIT 72`;
+const qryB = `SELECT onshorewindpower + offshorewindpower AS windpower, hourdk AS time FROM energydata WHERE pricearea = 'DK1' OFFSET 20 LIMIT 12`;
 //const qryB = `SELECT fossilgas + fossilhardcoal + fossiloil as notgreen, biomass + hydropower + otherrenewable + solarpower + onshorewindpower + offshorewindpower AS green FROM energydata WHERE pricearea = 'DK1' LIMIT 10`;
-const qryC = `SELECT EXTRACT(HOUR FROM hourdk) AS Time, onshorewindpower + offshorewindpower AS Windpower, hydropower AS hydro, solarpower, fossilgas + fossilhardcoal + fossiloil as fossil FROM energydata WHERE pricearea = 'DK1' OFFSET  9 LIMIT 1`;
-const qryD = `SELECT EXTRACT(HOUR FROM hourdk) AS Time, onshorewindpower + offshorewindpower AS Windpower, hydropower AS hydro, solarpower, fossilgas + fossilhardcoal + fossiloil as fossil FROM energydata WHERE pricearea = 'DK2' OFFSET  9 LIMIT 1`;
-const qryE = `SELECT EXTRACT(HOUR FROM hourdk) AS Time, onshorewindpower + offshorewindpower AS Windpower, hydropower AS hydro, solarpower, biomass, fossilgas + fossilhardcoal + fossiloil as fossil FROM energydata WHERE pricearea = 'DK1' OFFSET  9 LIMIT 24`;
-const qryF = `SELECT EXTRACT(HOUR FROM hourdk) AS Time, onshorewindpower + offshorewindpower AS Windpower, hydropower AS hydro, solarpower, biomass, fossilgas + fossilhardcoal + fossiloil as fossil FROM energydata WHERE pricearea = 'DK2' OFFSET  9 LIMIT 24`;
+const qryC = `SELECT EXTRACT(HOUR FROM hourdk) AS Time, onshorewindpower + offshorewindpower AS Windpower, hydropower AS hydro, solarpower, fossilgas + fossilhardcoal + fossiloil as fossil FROM energydata WHERE pricearea = 'DK1' OFFSET  20 LIMIT 1`;
+const qryD = `SELECT EXTRACT(HOUR FROM hourdk) AS Time, onshorewindpower + offshorewindpower AS Windpower, hydropower AS hydro, solarpower, fossilgas + fossilhardcoal + fossiloil as fossil FROM energydata WHERE pricearea = 'DK2' OFFSET  20 LIMIT 1`;
+const qryE = `SELECT EXTRACT(HOUR FROM hourdk) AS Time, onshorewindpower + offshorewindpower AS Windpower, hydropower AS hydro, solarpower, biomass, fossilgas + fossilhardcoal + fossiloil as fossil FROM energydata WHERE pricearea = 'DK1' OFFSET  20 LIMIT 24`;
+const qryF = `SELECT EXTRACT(HOUR FROM hourdk) AS Time, onshorewindpower + offshorewindpower AS Windpower, hydropower AS hydro, solarpower, biomass, fossilgas + fossilhardcoal + fossiloil as fossil FROM energydata WHERE pricearea = 'DK2' OFFSET  20 LIMIT 24`;
 
 app.use(
   cors({
